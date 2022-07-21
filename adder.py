d16da1d722c730ed5ac5816ab962bf52c3d1e356
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-07-21 22:56:22 +0530

    Update adder.py

--- adder.py
+++ adder.py
@@ -5,7 +5,7 @@ import time
 
 Alf = Alpha("yashu-alpha", api_id = API_ID, api_hash = API_HASH, session_string = STRING_SESSION)
 
-@Alf.on_message(filters.command("addall", "."))
+@Alf.on_message(filters.command("addall", "!"))
 async def add(_, m):
     l = m.chat.id
     try:
