ced7d10cee4670d1ed36eecc958c8018368d8412
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-07-21 12:15:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 API_ID = int(os.environ['API_ID'])
 API_HASH = os.environ['API_HASH']
-BOT_TOKEN = os.environ['BOT_TOKEN']
+STRING_SESSION = os.environ['STRING_SESSION']
 SUDO = os.environ['SUDO USERS']
