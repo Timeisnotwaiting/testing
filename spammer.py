15f6b820a0c304d815351b821b0a00a4cd19189b
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-07-25 12:06:43 +0530

    Update spammer.py

--- spammer.py
+++ spammer.py
@@ -7,10 +7,18 @@ import datetime
 
 Alf = Alpha("yashu-alpha", api_id = API_ID, api_hash = API_HASH, session_string = STRING_SESSION)
 
+@Alf.on_message(filters.command("spam", "!") & ~filters.edited)
+async def spammer(_, m):
+    if not m.from_user.id in SUDO:
+        return
+    hehe = m.text.split(None, 3)
+    counter = hehe[1]
+    txt = hehe[3]
+    delay = hehe[2]
+    for alpha in range(0, counter):
+        await _.send_message(m.chat.id, txt)
+        time.sleep(delay)
 
-
-
-    
 
 if YA == "YashuAlpha":
     Alf.run()
