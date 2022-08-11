e000a6d511c7d9e918ecc76aeb9c5abe48d0b5f9
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-08-11 22:31:28 +0530

    Delete helper.py

--- helper.py
+++
@@ -1,9 +0,0 @@
-from pyrogram import Client, filters
-from pyrogram.types import Message
-
-async def eor(_, m, t):
-    try:
-        await m.edit(t)
-    except:
-        await m.reply(t)
-        await m.delete()
