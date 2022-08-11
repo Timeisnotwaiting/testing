6e765245b47ec6374f52cc3b0509072b2fcc85e4
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-08-11 22:33:52 +0530

    Update spammer.py

--- spammer.py
+++ spammer.py
@@ -1,67 +1,99 @@
-from pyrogram import Client as Alpha, filters, idle
+from pyrogram import Client, filters, idle
 from pyrogram.types import Message
 from config import *
-import time
-from helper import eor
+from helper import get_id
+from database.client import *
 
-Alf = Alpha("yashu-alpha", api_id = API_ID, api_hash = API_HASH, session_string = STRING_SESSION)
 
-stop = False
-spam = False
+alpha = Client(":Alpha:", API_ID, API_HASH, BOT_TOKEN)
 
-@Alf.on_message(filters.command("spam", "!"))
-async def spammer(_, m):
-    global spam
-    global stop
-    try:
-        SUDO.append(str(level))
-    except:
-        pass
-    if not str(m.from_user.id) in SUDO:
+@alpha.on_message(filters.command(["gmute", "ungmute"]))
+async def gmute(_, m):
+    sudo = await is_sudo(m.from_user.id)
+    if not sudo:
         return
-    if len(m.command) < 4:
-        return await eor(_, m, f"<code>Usage: !spam < count > < delay > < text ></code>")
-    hehe = m.text.split(None, 3)
-    try:
-        counter = int(hehe[1])
-        txt = hehe[3]
-        delay = int(hehe[2])
-    except:
-        return await eor(_, m, f"<code>Usage: !spam < count > < delay > < text ></code>")
-    for alpha in range(0, int(counter)):
-        if stop:
-            stop = False
-            spam = False
-            return
-        await _.send_message(m.chat.id, txt)
-        time.sleep(int(delay))
-        spam = True
-    spam = False
+    id, r = await get_id(_, m)
+    if not id:
+        return await m.reply(r)
+    if id == bot_id:
+        return await m.reply("You can't mute self bot.... ")
+    sudo_check = await is_sudo(id)
+    if sudo_check:
+        return await m.reply("Can't gmute sudo users.... !")
+    muted = await is_muted(id)
+    if m.text.split()[0][1].lower() == "u":
+        if muted:
+            ok = await m.reply("unmuting user.... ")
+            try:
+                await unmute_user(id)
+                return await ok.edit(f"{(await _.get_users(id)).mention} unmuted !")
+            except:
+                return await ok.edit("Error at database !")
+        else:
+            return await m.reply("This user is not muted.... !")
+    if not muted:
+        ok = await m.reply("muting user.... ")
+        try:
+            await mute_user(id)
+            return await ok.edit(f"{(await _.get_users(id)).mention} gmuted !")
+        except Exception as e:
+            return await ok.edit(f"can't add user id to database...\n\nError :- {e}")
+    else:
+        return await m.reply("This user is already gmuted.... !")
+    
+    
+@alpha.on_message(group=1)
+async def cwf(_, m):
+    MUTED = await get_muted()
+    if m.from_user.id in MUTED:
+        try:
+            return await m.delete()
+        except:
+            return 
 
-@Alf.on_message(filters.command("endspam", "!"))
-async def spamend(_, m):
-    global spam
-    global stop
-    try:
-        SUDO.append(str(level))
-    except:
-        pass
-    if not str(m.from_user.id) in SUDO:
+@alpha.on_message(filters.command(["addsudo", "delsudo"]))
+async def sudo(_, m):
+    sudo = await is_sudo(m.from_user.id)
+    if not sudo:
         return
-    if not spam:
-        return await eor(_, m, "<code>No process running.....</code>")
-    await eor(_, m, f"<code>terminating process....</code>")
-    stop = True
-    await eor(_, m, f"<code>Spam ended....</code>")
+    id, r = await get_id(_, m)
+    if not id:
+        return await m.reply(r)
+    if id == m.from_user.id:
+        return await m.reply("You can't add / del self as sudo...")
+    if id == bot_id:
+        return await m.reply("You can't add self as sudo.... ")
+    muted = await is_muted(id)
+    sudo = await is_sudo(id)
+    if is_sudo:
+        if m.text.split()[0][1].lower() == "d":
+            await del_sudo(id)
+            return await m.reply("removed sudo ....")
+    men = (await _.get_users(id)).mention
+    if not sudo:
+        await add_sudo(id)
+        if muted:
+            await unmute_user(id)
+        return await m.reply(f"{men} is added as sudo.. !")
+    return await m.reply("This user is already a sudo...")
+
+@alpha.on_message(filters.command("sudos"))
+async def get_s(_, m):
+    sudo = await is_sudo(m.from_user.id)
+    if not sudo:
+        return
+    msg = ""
+    SUDOS = await get_sudos()
+    for sudos in SUDOS:
+        sudos = str(sudos)
+        msg += f"\n{sudos}"
+    final = f"List of sudo :- \n{msg}"
+    return await m.reply(final)
     
 
-if not YA == "YashuAlpha":
-    print("Password wrong !")
-else:
-    Alf.start()
-    me = Alf.get_me()
-    level = me.id
-    uname = me.username
-    idle()
-    print(f"@{uname if uname else None} started successfully... ")
+alpha.run()
+me = alpha.get_me()
+username = me.username
+bot_id = me.id
 
+print(f"@{username if username else None} started successfully... !")
