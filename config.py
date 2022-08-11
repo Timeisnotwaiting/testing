58a917c7dd28db2c7fb5e15a686aa2d4033d9a35
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-08-11 22:32:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,6 @@
-import os
+import os 
 
-API_ID = int(os.environ['API_ID'])
+API_ID = os.environ['API_ID']
 API_HASH = os.environ['API_HASH']
-STRING_SESSION = os.environ['STRING_SESSION']
-SUDO_USERS = os.environ['SUDO_USERS'].split()
-YA = os.environ['PASSWORD']
-SUDO = SUDO_USERS
-
+BOT_TOKEN = os.environ['BOT_TOKEN']
+MONGO_DB_URL = os.environ['MONGO_DB_URL']
